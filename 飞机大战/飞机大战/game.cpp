84486365fee7e1d4847b9fbc1061cdb2fb21de07
Yuehui Zhong <1957310740@qq.com>
2026-05-07 18:00:50 +0800

    完善射击,修改子弹图片,添加技能函数

--- 飞机大战/飞机大战/game.cpp
+++ 飞机大战/飞机大战/game.cpp
@@ -1,10 +1,12 @@
-#include "game.h"
+﻿#include "game.h"
 #include "resource.h"
 #include <iostream>
-
 #pragma comment(lib, "msimg32.lib")
 using namespace std;
-
+// ======================================
+// 全局变量
+// ======================================
+int g_kill_count = 0;
 // ======================================
 // 全局函数实现
 // ======================================
@@ -21,6 +23,17 @@ void init()
 {
     srand((unsigned)time(NULL));
     initgraph(WINDOWS_WIDTH, WINDOWS_HEIGHT);
+
+    // 让游戏窗口屏幕居中
+    HWND hWnd = GetHWnd();   // 获取EasyX窗口句柄
+    int sceenW = GetSystemMetrics(SM_CXSCREEN);
+    int sceenH = GetSystemMetrics(SM_CYSCREEN);
+    // 计算居中坐标
+    int winX = (sceenW - WINDOWS_WIDTH) / 2;
+    int winY = (sceenH - WINDOWS_HEIGHT) / 2-50;
+    // 设置窗口固定到居中位置
+    SetWindowPos(hWnd, NULL, winX, winY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+
     BeginBatchDraw();
     Player_plane::load_image();
     EnemyA_plane::load_image();
@@ -37,37 +50,82 @@ void main_loop()
     vector<Bullet*> bullet_list;
     vector<Plane*> plane_list;
     loadimage(&background, RT_RCDATA, MAKEINTRESOURCE(IDB_BACKGROUND), WINDOWS_WIDTH, WINDOWS_HEIGHT);
-    Player_plane player(WINDOWS_WIDTH / 2, WINDOWS_HEIGHT - 100, 100);
-    EnemyA_plane enemya1(int_dist(gen), 0, 100);
-    EnemyA_plane enemya2(int_dist(gen), 0, 100);
-    EnemyA_plane enemya3(int_dist(gen), 0, 100);
-    plane_list.push_back(&player);
-    plane_list.push_back(&enemya1);
-    plane_list.push_back(&enemya2);
-    plane_list.push_back(&enemya3);
+    Player_plane* player = new Player_plane(WINDOWS_WIDTH / 2, WINDOWS_HEIGHT - 100);
+    plane_list.push_back(player);
     while (1)
     {
+        while (plane_list.size() < 10)
+        {
+            plane_list.push_back(new EnemyA_plane(int_dist(gen), 0));
+        }
         total_move(bullet_list,plane_list);
-        total_check_side(bullet_list, plane_list);
         total_shoot(bullet_list, plane_list);
+        collision(bullet_list, plane_list);
+        total_skill(bullet_list, plane_list);
+        total_check_side(bullet_list, plane_list);
+        total_cleanup(bullet_list, plane_list);
         total_draw(bullet_list, plane_list,background);
         Sleep(16);
     }
 }
 
-void collision()
+void collision(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list)
 {
-    
+    vector<Bullet*>::iterator b_it = bullet_list.begin();
+
+    while (b_it != bullet_list.end())
+    {
+        vector<Plane*>::iterator p_it = plane_list.begin(); // 每次重置飞机迭代器
+
+        while (p_it != plane_list.end())
+        {
+            if ((*b_it)->alive==false)
+            {
+                break;
+            }
+            if ((*p_it)->HP <= 0)
+            {
+                ++p_it;
+                continue;
+            }
+            bool is_collide =
+                ((*p_it)->is_player != (*b_it)->is_player)&&//敌我区分
+                (*b_it)->pos[0] < (*p_it)->pos[0] + (*p_it)->width &&
+                (*b_it)->pos[0] + (*b_it)->width > (*p_it)->pos[0] &&
+                (*b_it)->pos[1] < (*p_it)->pos[1] + (*p_it)->height &&
+                (*b_it)->pos[1] + (*b_it)->height > (*p_it)->pos[1];
+            if (is_collide)
+            {
+                (*b_it)->alive = false;
+                (*p_it)->HP -= 20;
+                cout << "碰撞发生！飞机剩余血量：" << (*p_it)->HP << endl;
+                break;
+            }
+            ++p_it;
+        }
+            ++b_it;
+    }
 }
+
 
 void total_draw(vector<Bullet*>& bullet_list,vector<Plane*>& plane_list,IMAGE& background)
 {
     cleardevice();//清空
     putimage(0, 0, &background);
+    //显示击杀数
+    settextstyle(50, 0, _T("微软雅黑"));//字体
+    settextcolor(RED);//颜色
+    setbkmode(TRANSPARENT);//透明底部
+    // 拼接文字 + 数字
+    string kill_text = "击杀数:" + to_string(g_kill_count);
+    //显示
+    outtextxy(0,0, kill_text.c_str());
+    // 显示子弹
     for (Bullet* b : bullet_list)
     {
         b->draw();
     }
+    //显示飞机
     for (Plane* p : plane_list)
     {
         p->draw();
@@ -75,7 +133,7 @@ void total_draw(vector<Bullet*>& bullet_list,vector<Plane*>& plane_list,IMAGE& b
     FlushBatchDraw();//加载缓存区图片到窗口
 }
 
-void total_move(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list)
+void total_move(vector<Bullet*>& bullet_list,vector<Plane*>& plane_list)
 {
     for (Bullet* b : bullet_list)
     {
@@ -89,10 +147,12 @@ void total_move(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list)
 
 void total_check_side(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list)
 {
-    for (Bullet* b : bullet_list)
+    // 子弹检查
+    for (Bullet* b:bullet_list)
     {
-        //b->check_side();
+        b->check_side();
     }
+    // 飞机检查
     for (Plane* p : plane_list)
     {
         p->check_side();
@@ -106,12 +166,67 @@ void total_shoot(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list)
         p->shoot(bullet_list);
     }
 }
+
+void total_cleanup(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list)
+{
+    //清理死掉的子弹
+    vector<Bullet*>::iterator b_it = bullet_list.begin();
+    while (b_it != bullet_list.end())
+    {
+        if ((*b_it)->alive == false)
+        {
+            delete* b_it;
+            b_it = bullet_list.erase(b_it);
+        }
+        else
+        {
+            ++b_it;
+        }
+    }
+    //清理死掉的飞机
+    vector<Plane*>::iterator p_it = plane_list.begin();
+    while (p_it != plane_list.end())
+    {
+        if ((*p_it)->is_player && (*p_it)->HP <= 0)
+        {
+            //cout << "玩家阵亡！游戏结束！按任意键退出..." << endl;
+            delete (*p_it);
+            p_it = plane_list.erase(p_it);
+        }
+        if (!(*p_it)->is_player && (*p_it)->HP <= 0)
+        {
+            //cout << "敌机被击毁！" << endl;
+            delete (*p_it);
+            p_it = plane_list.erase(p_it);
+            g_kill_count++;
+        }
+        else
+        {
+            ++p_it;
+        }
+    }
+}
+
+void total_skill(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list)
+{
+    for (Plane* p : plane_list)
+    {
+        p->skill();
+    }
+}
 // ======================================
 // Plane基类实现
 // ======================================
-Plane::Plane(int x, int y, int hp, IMAGE* pimg)
+Plane::Plane(bool Is_player,int Shoot_cd,int x, int y, int hp, IMAGE* pimg)
 {
+    is_player = Is_player;
+    shoot_cd = Shoot_cd;
+    current_skill_cd = 0;
+    skill_cd = 60;
+    duration_cd= 0;
     HP = hp;
+    duration_cd = 0;
+    current_shoot_cd= 0;
     pos[0] = x;
     pos[1] = y;
     height = 60;
@@ -130,6 +245,22 @@ void Plane::move()
 void Plane::draw()
 {
     draw_transparent(pos[0], pos[1], img);
+    //设置文字参数
+    settextstyle(20, 0, _T("微软雅黑"));//字体
+    if (is_player == true)
+    {
+        settextcolor(YELLOW);//颜色
+    }
+    else
+    {
+        settextcolor(WHITE);//颜色
+    }
+    setbkmode(TRANSPARENT);//透明底部
+    // 拼接文字 + 数字
+    string hp_text = "HP:" + to_string(HP);
+    //显示
+    outtextxy(pos[0],pos[1], hp_text.c_str());
+
 }
 
 void Plane::check_side()
@@ -139,9 +270,17 @@ void Plane::check_side()
     if (pos[1] < 0) pos[1] = 0;
     if (pos[1] + height > WINDOWS_HEIGHT) pos[1] = WINDOWS_HEIGHT - height;
 }
+
 void Plane::shoot(vector<Bullet*>& bullet_list)
 {
+
 }
+
+void Plane::skill()
+{
+
+}
+
 // ======================================
 // Player_plane实现
 // ======================================
@@ -152,15 +291,33 @@ void Player_plane::load_image()
     loadimage(&player_img, RT_RCDATA, MAKEINTRESOURCE(IDB_PLAYER), 60, 60);
 }
 
-Player_plane::Player_plane(int x, int y, int hp) : Plane(x, y, hp, &player_img) {}
+Player_plane::Player_plane(int x, int y) : Plane(true,10,x, y,100, &player_img){}
 
 void Player_plane::shoot(vector<Bullet*>& bullet_list)
 {
-    if (GetAsyncKeyState('J') & 0x8000)
+    if (current_shoot_cd > 0)
+    {
+        current_shoot_cd--;
+    }
+    if (GetAsyncKeyState('J') & 0x8000 && current_shoot_cd==0)
     {
         int bullet_x = pos[0] + width / 2;
         int bullet_y = pos[1];
-        bullet_list.push_back(new Bullet_normal_player(true, bullet_x, bullet_y));
+        bullet_list.push_back(new Bullet_normal_player(bullet_x, bullet_y));
+        current_shoot_cd = shoot_cd;
+    } 
+}
+
+void Player_plane::skill()
+{
+    if (current_skill_cd > 0)
+    {
+        current_skill_cd--;
+    }
+    if (GetAsyncKeyState('K') & 0x8000 && current_skill_cd == 0)
+    {
+        shoot_cd = shoot_cd / 2;
+        current_skill_cd = skill_cd;
     }
 }
 // ======================================
@@ -173,7 +330,7 @@ void EnemyA_plane::load_image()
     loadimage(&player_img, RT_RCDATA, MAKEINTRESOURCE(IDB_ENEMYA), 60, 60);
 }
 
-EnemyA_plane::EnemyA_plane(int x, int y, int hp) : Plane(x, y, hp, &player_img)
+EnemyA_plane::EnemyA_plane(int x, int y) : Plane(false,60,x, y, 100, &player_img)
 {
     direction_y = (rand() % 2 == 0 ? -1 : 1);
     direction_x = (rand() % 2 == 0 ? -1 : 1);
@@ -198,19 +355,31 @@ void EnemyA_plane::move()
 
 void EnemyA_plane::shoot(vector<Bullet*>& bullet_list)
 {
-    if (direction_y == -1)
+    if (current_shoot_cd > 0)
+    {
+        current_shoot_cd--;
+    }
+    if (direction_y == -1 && current_shoot_cd==0)
     {
         int bullet_x = pos[0] + width / 2-10;
         int bullet_y = pos[1]+height;
-        bullet_list.push_back(new Bullet_normal_enemy(true, bullet_x, bullet_y));
+        bullet_list.push_back(new Bullet_normal_enemy(bullet_x, bullet_y));
+        current_shoot_cd = shoot_cd;
     }
 }
+
+void EnemyA_plane::skill()
+{
+    //敌机A暂时不设置技能
+}
+
 // ======================================
 // Bullet基类实现
 // ======================================
-Bullet::Bullet(bool Alive, int x, int y, IMAGE* pimg)
+Bullet::Bullet(bool Alive,bool Is_player, int x, int y, IMAGE* pimg)
 {
     alive = Alive;
+    is_player = Is_player;
     pos[0] = x;
     pos[1] = y;
     width = 20;
@@ -218,7 +387,20 @@ Bullet::Bullet(bool Alive, int x, int y, IMAGE* pimg)
     img = pimg;
 }
 
-void Bullet::move() {}
+void Bullet::move()
+{
+
+}
+
+void Bullet::check_side()
+{
+    // 检查子弹是否在窗口内
+    // 返回true表示在边界内（活着），返回false表示超出边界（应删除)
+    if (pos[0] + width < 0 || pos[0] > WINDOWS_WIDTH || pos[1] + height < 0 || pos[1] > WINDOWS_HEIGHT)
+    {
+        alive=false;
+    }
+}
 void Bullet::draw()
 {
     draw_transparent(pos[0], pos[1], img);
@@ -231,14 +413,14 @@ IMAGE Bullet_normal_player::bullet_normal_player_img;
 
 void Bullet_normal_player::load_image()
 {
-    loadimage(&bullet_normal_player_img, RT_RCDATA, MAKEINTRESOURCE(IDB_BULLET), 20, 40);
+    loadimage(&bullet_normal_player_img, RT_RCDATA, MAKEINTRESOURCE(IDB_PLAYER_BULLET_NORMAL), 10, 30);
 }
 
-Bullet_normal_player::Bullet_normal_player(bool alive,int x, int y) : Bullet(alive, x, y, &bullet_normal_player_img) {}
+Bullet_normal_player::Bullet_normal_player(int x, int y) : Bullet(true,true,x, y, &bullet_normal_player_img) {}
 
 void Bullet_normal_player::move()
 {
-    pos[1] -= 10;
+    pos[1] -= 20;
 }
 
 // ======================================
@@ -248,12 +430,12 @@ IMAGE Bullet_normal_enemy::bullet_normal_enemy_img;
 
 void Bullet_normal_enemy::load_image()
 {
-    loadimage(&bullet_normal_enemy_img, RT_RCDATA, MAKEINTRESOURCE(IDB_BULLET), 20, 40);
+    loadimage(&bullet_normal_enemy_img, RT_RCDATA, MAKEINTRESOURCE(IDB_ENEMY_BULLET_NORMAL), 20, 20);
 }
 
-Bullet_normal_enemy::Bullet_normal_enemy(bool alive, int x, int y) : Bullet(alive, x, y, &bullet_normal_enemy_img) {}
+Bullet_normal_enemy::Bullet_normal_enemy(int x, int y) : Bullet(true,false,x, y, &bullet_normal_enemy_img) {}
 
 void Bullet_normal_enemy::move()
 {
-    pos[1] += 10;
+    pos[1] += 15;
 }
--- 飞机大战/飞机大战/game.h
+++ 飞机大战/飞机大战/game.h
@@ -1,15 +1,18 @@
-#pragma once
+﻿#pragma once
 #include <easyx.h>
 #include <Windows.h>
 #include <random>
 #include <cstdlib>
 #include <ctime>
+#include<string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 // 宏定义
-#define WINDOWS_WIDTH  800
+#define WINDOWS_WIDTH  900
 #define WINDOWS_HEIGHT 900
+
 class Bullet;
 class Plane;
 // ======================================
@@ -23,11 +26,19 @@ public:
     int pos[2];
     int width;
     int height;
-    Plane(int x, int y, int hp, IMAGE* pimg);
+    bool is_player;//是否是玩家飞机
+    int shoot_cd;//射击冷却
+    int current_shoot_cd;//射击剩余冷却
+    int skill_cd;//技能冷却
+    int current_skill_cd;//技能剩余冷却
+    int duration_cd;//技能持续时长
+    Plane(bool Is_player,int Shoot_cd,int x, int y, int hp, IMAGE* pimg);
+    virtual ~Plane() = default;
     virtual void move();//移动函数
     virtual void draw();//绘制函数
     void check_side();//边界检查
     virtual void shoot(vector<Bullet*>& bullet_list);//射击函数
+    virtual void skill();//技能函数
 };
 
 // ======================================
@@ -38,8 +49,9 @@ class Player_plane : public Plane
 public:
     static IMAGE player_img;
     static void load_image();
-    Player_plane(int x, int y, int hp);
+    Player_plane(int x, int y);
     void shoot(vector<Bullet*>& bullet_list) override;
+    virtual void skill() override;
 };
 
 // ======================================
@@ -53,9 +65,10 @@ public:
     int direction_x;
     int flash_count;
     static void load_image();
-    EnemyA_plane(int x, int y, int hp);
+    EnemyA_plane(int x, int y);
     void move() override;
     void shoot(vector<Bullet*>& bullet_list) override;
+    virtual void skill() override;
 };
 
 // ======================================
@@ -66,12 +79,15 @@ class Bullet
 public:
     IMAGE* img;
     bool alive;
+    bool is_player;
     int pos[2];
     int width;
     int height;
-    Bullet(bool Alive, int x, int y, IMAGE* pimg);
+    Bullet(bool Alive,bool Is_player, int x, int y, IMAGE* pimg);
+    virtual ~Bullet() = default;
     virtual void move();
     virtual void draw();
+    void check_side();
 };
 
 // ======================================
@@ -82,7 +98,7 @@ class Bullet_normal_player: public Bullet
 public:
     static IMAGE bullet_normal_player_img;
     static void load_image();
-    Bullet_normal_player(bool alive,int x, int y);
+    Bullet_normal_player(int x, int y);
     void move() override;
 };
 
@@ -94,15 +110,17 @@ class Bullet_normal_enemy : public Bullet
 public:
     static IMAGE bullet_normal_enemy_img;
     static void load_image();
-    Bullet_normal_enemy(bool alive, int x, int y);
+    Bullet_normal_enemy(int x, int y);
     void move() override;
 };
 // 全局函数声明
 void draw_transparent(int x, int y, IMAGE* img);//实现飞机背景图透明
 void init();//初始化
 void main_loop();//主循环
-void collision();//碰撞函数
+void collision(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list);//碰撞函数
 void total_draw(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list, IMAGE& background);//一次性绘制函数
 void total_move(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list);//一次性移动函数
 void total_check_side(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list);//一次性边界检查函数
 void total_shoot(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list);//一次性射击
+void total_cleanup(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list);//一次性清理函数
+void total_skill(vector<Bullet*>& bullet_list, vector<Plane*>& plane_list);//一次性技能函数
